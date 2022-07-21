887da60146ad0e0132a5b951475dda38653d903c
Simbaclaws <simbaclaws@Kali>
2022-07-21 18:23:25 +0200

    cyberpanel.sh test

--- cyberpanel.sh
+++ cyberpanel.sh
@@ -72,9 +72,11 @@ Set_Default_Variables() {
 	Temp_Value=$(curl --silent --max-time 30 -4 "$RAW_GIT_REPO/$BRANCH_NAME/version.txt")
 	Panel_Version=$( echo "$Temp_Value" | jq -r '.version')
 	Panel_Build=$( echo "$Temp_Value" | jq -r '.build')
-
+	echo $Temp_Value
+	echo $Panel_Version
 	Branch_Name="v${Panel_Version}.${Panel_Build}"
-
+	echo $Branch_Name
+	echo "$RAW_GIT_REPO/$BRANCH_NAME/version.txt"
 	if [[ $Branch_Name = v*.*.* ]] ; then
 		echo -e  "\nBranch name fetched...$Branch_Name"
 	elif [[ $Branch_Name = "vstable." ]] ; then
